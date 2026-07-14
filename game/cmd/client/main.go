eadba68648b36faa444ed2866b1231e7c7ee2030
Roman Reutov <peytob23@gmail.com>
2026-07-14 23:05:24 +0300

    engine-dependency: window OGL configuration

--- game/cmd/client/main.go
+++ game/cmd/client/main.go
@@ -62,9 +62,12 @@ func main() {
 	/* Modules */
 
 	w, err := window.Init(window.Opts{
-		Width:  800,
-		Height: 600,
-		Title:  "another-rpg",
+		Width:   800,
+		Height:  600,
+		Title:   "another-rpg",
+		GLMajor: 3,
+		GLMinor: 3,
+		Visible: true,
 	})
 	if err != nil {
 		panic("failed to initialize window module: " + err.Error())
